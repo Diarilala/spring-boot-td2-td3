15d3d2abf6e0e014004aec3b03bdd16cdcc765e5
Diarilala <hei.diarilala@gmail.com>
2026-03-23 18:37:46 +0300

    feat: add TD3 A-a answer(Correction)

--- src/main/java/com/example/spring_boot/controller/WelcomeController.java
+++ src/main/java/com/example/spring_boot/controller/WelcomeController.java
@@ -2,6 +2,8 @@ package com.example.spring_boot.controller;
 
 import com.example.spring_boot.service.WelcomeService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,15 @@ public class WelcomeController {
     private final WelcomeService welcomeService;
 
     @GetMapping("/welcome")
-    public String welcome(@RequestParam String name) {
-        return welcomeService.welcomeMessage(name);
+    public ResponseEntity<String> welcome(@RequestParam String name) {
+
+        if(name.isEmpty()){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("No name provided");
+        }
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Welcome " + name);
     }
 }
