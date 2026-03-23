6b977dbb01b2bf6b06d1ae592ee5f6e943f6e0e5
Diarilala <hei.diarilala@gmail.com>
2026-03-23 12:26:19 +0300

    feat: add TD2 C answer

--- src/main/java/com/example/spring_boot/controller/StudentController.java
+++ src/main/java/com/example/spring_boot/controller/StudentController.java
@@ -3,10 +3,7 @@ package com.example.spring_boot.controller;
 import com.example.spring_boot.model.Student;
 import com.example.spring_boot.service.StudentService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +19,13 @@ public class StudentController {
     public List<String> addStudents(@RequestBody List<Student> students) {
         studentService.addStudents(students);
         return studentService.getLastNames();
+    }
+
+    @GetMapping
+    public String getStudents(@RequestHeader("Accept") String acceptHeader) {
+        if (acceptHeader.equals("text/plain")) {
+            return String.join(", ", studentService.getLastNames());
+        }
+        return "Not supported format";
     }
 }
